83809f24be30c54e2a68ca004df8e0b96b6685a2
black-black-cat <applyk99@163.com>
2020-11-22 15:55:40 +0800

    update colors

--- code samples/js-sample.js
+++ code samples/js-sample.js
@@ -8,6 +8,7 @@ const temp = `123${window.location.origin + '?ans=123'}`
  * @param {string} url - The URL to request
  */
 function foo(bar) {
+  console.log(this)
   switch(bar) {
     case 'foo':
       return `values is: ${bar.length + '999'}`
